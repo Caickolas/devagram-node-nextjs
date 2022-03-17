66c3c7ad71fdcbdc698a9ef6e0db11f37527dbfb
Caickolas <88976384+Caickolas@users.noreply.github.com>
2022-03-17 15:13:48 -0300

    versao inicial login

---
+++ pages/api/login.ts
@@ -0,0 +1,20 @@
+import type {NextApiRequest, NextApiResponse} from "next";
+
+export default(
+    req : NextApiRequest,
+    res : NextApiResponse
+) => {
+    if(req.method === 'POST'){
+        const {login, senha} = req.body;
+
+        if(login === 'admin@admin.com' &&
+            senha === 'admin@123'){
+            res.status(200).json({ msg:'Usuario autenticado com sucesso' })
+
+            }
+        return res.status(400).json({ erro: 'Usuario ou senha não encontrados' })
+    }
+
+    return res.status(405).json({erro: 'Metodo informado não é valido'})
+}
+
