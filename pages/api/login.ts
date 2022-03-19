bb4ab8392effef38ca49b05e7661418b1e8315f7
Caickolas <88976384+Caickolas@users.noreply.github.com>
2022-03-19 20:40:28 -0300

    inicio jwt (problema na env jwt)

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,21 +1,35 @@
 import type {NextApiRequest, NextApiResponse} from "next";
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg'
+import md5 from "md5";
+import { UsuarioModel } from '../../models/UsuarioModel';
+import jwt from 'jsonwebtoken';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-const endpointLogin = (
+const endpointLogin = async (
     req : NextApiRequest,
-    res: NextApiResponse<RespostaPadraoMsg>
+    res: NextApiResponse<RespostaPadraoMsg | any>
 ) => {
-    if(req.method === 'POST'){
-        const {login, senha} = req.body;
 
-        if(login === 'admin@admin.com' &&
-            senha === 'admin@123'){
-            return res.status(200).json({ msg:'Usuario autenticado com sucesso' });
+    const { MINHA_CHAVE_JWT } = process.env;
+        if (!MINHA_CHAVE_JWT){
+        return res.status(500).json({erro : 'ENV Jwt nao informada'});
+    }
+        if(req.method === 'POST'){
+            const {login, senha} = req.body;
 
-            }
-        return res.status(400).json({ erro: 'Usuario ou senha não encontrados' });
+        const usuariosEncontrados = await UsuarioModel.find({email : login, senha : md5(senha)})
+        if(usuariosEncontrados && usuariosEncontrados.length > 0){
+            const usuarioEncontrado = usuariosEncontrados[0]
+
+
+            const token = jwt.sign({_id : usuarioEncontrado._id}, MINHA_CHAVE_JWT);
+            return res.status(400).json({
+                nome: usuarioEncontrado.nome,
+                email: usuarioEncontrado.email,
+                token
+            });
+        }
     }
 
     return res.status(405).json({erro: 'Metodo informado não é valido'});
