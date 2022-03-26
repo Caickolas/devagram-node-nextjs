ff6b52af1483bd9b5c7db810cc51cc8108bb0381
Caickolas <88976384+Caickolas@users.noreply.github.com>
2022-03-26 08:40:57 -0300

    api like/deslike

---
+++ pages/api/like.ts
@@ -0,0 +1,45 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
+import { validarTokenJWT } from "../../middlewares/validarTokenJWT";
+import { PublicacaoModel } from "../../models/PublicacaoModel";
+import { UsuarioModel } from "../../models/UsuarioModel";
+import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
+
+const likeEndpoint = async (req : NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) =>{
+    try{
+
+        if(req.method === 'PUT'){
+            const{id} = req?.query;
+            const publicacao = await PublicacaoModel.findById(id)
+            if(!publicacao){
+                return res.status(400).json({ erro: 'Publicação não existe' })
+            }
+
+            const {userId} = req?.query
+            const usuario = await UsuarioModel.findById(userId)
+            if(!usuario){
+                return res.status(400).json({ erro: 'Usuario não encontrado' })
+            }
+
+            const indexDoUsuarioNoLike = publicacao.likes.findIndex((e: any) => e.toString() === usuario._id.toString()) 
+
+            if(indexDoUsuarioNoLike != -1){
+                publicacao.likes.splice(indexDoUsuarioNoLike, 1)
+                await PublicacaoModel.findByIdAndUpdate({ _id: publicacao._id }, publicacao)
+                return res.status(200).json({ msg: 'publicacao descurtida com sucesso' })
+            }else {
+                publicacao.likes.push(usuario._id)
+                await PublicacaoModel.findByIdAndUpdate({_id : publicacao._id}, publicacao)
+                return res.status(200).json({msg: 'publicacao curtida com sucesso'})
+            }
+
+        }
+        return res.status(405).json({ erro: 'Metodo informado não é valido' })
+
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({erro : 'Ocorreu um erro ao curtir/descurtir uma publicação'})
+    }
+}
+
+export default validarTokenJWT(conectarMongoDB(likeEndpoint))
